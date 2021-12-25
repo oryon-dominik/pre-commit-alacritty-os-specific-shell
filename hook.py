2eeae61141212fe2fc8c379b3d8a34feebad50c5
oryon-dominik <oryon@cyberise.de>
2021-12-25 12:34:21 +0100

    os specific hook

--- hook.py
+++ hook.py
@@ -2,11 +2,18 @@
 # coding: utf-8
 
 import argparse
-
+import platform
 
 def main(argv=None):
     parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*')
     parser.add_argument('--windows', action='store_true', dest='windows', default=True)
     parser.add_argument('--posix', action='store_true', dest='posix', default=False)
     args = parser.parse_args(argv)
-    print("Debug hook!", args)
+    
+    # TODO: read the alacritty.yml and switch shell.program between args.windows and args.posix depending on the detected os
+    # program: 'C:\Program Files\PowerShell\7\pwsh.exe'
+    # program: /bin/fish
+    # winpty_backend: true
+
+    print("Debug hook!", platform.system(), args)
