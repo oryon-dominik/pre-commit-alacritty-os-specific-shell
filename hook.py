36ca5ccc4c7e1c8094d77ada318d481525d15786
oryon-dominik <oryon@cyberise.de>
2021-12-25 12:21:17 +0100

    add arguments parser

--- hook.py
+++ hook.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-def main():
-    print("Debug hook!")
+import argparse
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--windows', action='store_true', dest='windows', default=True)
+    parser.add_argument('--posix', action='store_true', dest='posix', default=False)
+    args = parser.parse_args(argv)
+    print("Debug hook!", args)
